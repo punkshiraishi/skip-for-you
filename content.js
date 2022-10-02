c73f00df9699eaa6cf2273e29b05df202704ff7f
白石祐大 <shiraishi_yuma@uniquevision.co.jp>
2022-10-02 19:47:55 +0900

    create: auto click skip ad button

--- content.js
+++ content.js
@@ -1,14 +1,8 @@
-// 自由に DOM にアクセスできる
-chrome.runtime.onMessage.addListener((request, options) => {
-  console.log('contentscript')
-  console.log(options)
-
-  if (request.name === 'displayUrl:contentScripts') {
-    const body = document.querySelector('body')
-    const addElement = document.createElement('h1');
-
-    addElement.textContent = `URL is ${ request.data.url }`;
-    
-    body.prepend(addElement)
+const observer = new MutationObserver(() => {
+  const button = document.querySelector('.ytp-ad-skip-button');
+  if (button) {
+    button.click();
   }
 });
+
+observer.observe(document.body, { childList: true, subtree: true }); 
--- popup.js
+++
@@ -1,4 +0,0 @@
-const button = document.getElementById('display')
-button.addEventListener('click', () => {
-  chrome.runtime.sendMessage({ name: 'displayUrl:background' })
-})
